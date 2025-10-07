13dd7a5729968516137aad8be3591fb8cba0f271
Alhasan Mohsen <33701523+IAmFaulty@users.noreply.github.com>
2025-10-07 10:17:05 -0400

    Fix context

--- src/main/java/com/revature/training/springassignment/App.java
+++ src/main/java/com/revature/training/springassignment/App.java
@@ -8,10 +8,12 @@ public class App
     public static void main( String[] args )
     {
         ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-        MessageService emailService = (MessageService) context.getBean("emailService");
-        MessageService smsService = (MessageService) context.getBean("smsService");
+        MessageService emailService = context.getBean("emailService", MessageService.class);
+        MessageService smsService = context.getBean("smsService", MessageService.class);
 
         emailService.sendMessage();
         smsService.sendMessage();
+
+        ((ClassPathXmlApplicationContext) context).close();
     }
 }
